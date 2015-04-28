c0af60d4bb0754a79460b654b2aa577fd9f0f130
Saji Venugopalan <vsaji79@yahoo.com>
2015-04-28 23:07:38 +0800

    new exception class

---
+++ src/com/saji/lift/exercises/ProcessingException.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.saji.lift.exercises;
+
+/**
+ * @author Saji Venugopalan
+ *
+ */
+public class ProcessingException extends Exception {
+
+	/**
+	 * 
+	 */
+	public ProcessingException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public ProcessingException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public ProcessingException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public ProcessingException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 */
+	public ProcessingException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
